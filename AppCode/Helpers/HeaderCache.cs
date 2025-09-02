0318f1e6d0632421d4f5fe2e4a056e2ea761df05
Roman Opalko <roman.opalko@2sic.com>
2025-09-02 16:00:08 +0200

    wip: flushing

---
+++ AppCode/Helpers/HeaderCache.cs
@@ -0,0 +1,19 @@
+using System.Web;
+using System;
+
+namespace AppCode.Helpers
+{
+    public class HeaderCache : Custom.Hybrid.CodeTyped
+    {
+        public static void SetPublicCacheHeaders(HttpResponseBase Response, int browserCacheSeconds, int cloudflareCacheSeconds)
+        {
+            Response.Cache.SetCacheability(HttpCacheability.Public);
+            var ttlBrowser = TimeSpan.FromSeconds(browserCacheSeconds);
+            var ttlCloudflare = TimeSpan.FromSeconds(cloudflareCacheSeconds);
+            // Response.Cache.SetValidUntilExpires(true);
+            Response.Cache.SetMaxAge(ttlBrowser);
+            Response.Cache.SetProxyMaxAge(ttlCloudflare);
+            // Response.Cache.SetExpires(DateTime.UtcNow.Add(ttl));
+        }
+    }
+}
