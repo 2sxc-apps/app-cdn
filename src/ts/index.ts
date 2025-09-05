c04e1138bab56959edd850dbfc045becd0b639cb
Roman Opalko <roman.opalko@2sic.com>
2025-09-05 16:23:37 +0200

    moved parts to ts

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,39 +1,45 @@
-import Cloudflare from 'cloudflare';
-
-console.log('Cloudflare');
-
-const customPurgeBtn = document.querySelector('#custom-purge');
-if (customPurgeBtn) {
-  customPurgeBtn.addEventListener('click', function () {
-    const customUrl = (document.querySelector('#custom-purge-url') as HTMLInputElement)?.value;
-    purgeCache(customUrl);
-  });
-}
-
-const fullPurgeBtn = document.querySelector('#full-purge');
-if (fullPurgeBtn) {
-  fullPurgeBtn.addEventListener('click', function () {
-    purgeCache();
-  });
-}
-
-async function purgeCache(flushUrl: string = '') {
-  const client = new Cloudflare({
-    apiToken: '', // Use 'token' property, not 'apiToken'
-  });
-
-  // Replace with your actual zone ID
-  const zoneId = '';
-
-  let response;
-
-  if (!flushUrl) {
-    // Purge everything
-    response = await client.cache.purge({ zone_id: zoneId, purge_everything: true });
-  } else {
-    // Purge specific URL
-    response = await client.cache.purge({ zone_id: zoneId, files: [flushUrl] });
+document.addEventListener('DOMContentLoaded', () => {
+  const customPurgeBtn = document.querySelector('#custom-purge');
+  if (customPurgeBtn) {
+    customPurgeBtn.addEventListener('click', function () {
+      const customUrl = (document.querySelector('#custom-purge-url') as HTMLInputElement).value;
+      purgeCache(customUrl);
+    });
   }
 
-  console.log(response);
+  const fullPurgeBtn = document.querySelector('#full-purge');
+  if (fullPurgeBtn) {
+    fullPurgeBtn.addEventListener('click', function () {
+      purgeCache();
+    });
+  }
+
+  const tagPurgeBtn = document.querySelector('#tag-purge');
+  if (tagPurgeBtn) {
+    tagPurgeBtn.addEventListener('click', function () {
+      const tags = (document.querySelector('#tag-purge-url') as HTMLInputElement).value;
+      if (tags.trim() !== '') {
+        purgeCache('', tags);
+      } else {
+        alert('Please enter at least one tag to purge.');
+      }
+    });
+  }
+});
+
+function purgeCache(url = '', tags = '') {
+  if (!tags && (!url || url.trim() === '')) {
+    if (!confirm('You are about to purge the entire cache. Are you sure?')) return;
+  }
+
+  var sxc = ($2sxc as any)(6923);
+  var urlParams = {};
+  var postParams = {
+    flushUrl: url,
+    tags: tags,
+  };
+
+  sxc.webApi.post('app/auto/api/Cache/Purge', urlParams, postParams).then((data: any) => {
+    console.log(data);
+  });
 }
